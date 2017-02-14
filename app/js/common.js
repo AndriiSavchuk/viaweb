1e47f975bcc4a5e4349a8df4019f6ae815d1b201
Andrii Romanovuch <Andrii Romanovuch>
2017-02-14 20:01:55 +0200

    Added section Team

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,27 @@
-$(function() {
+$(document).ready(function() {
+
+    /* Fixed navigation */
 
     $(window).scroll(function () {
 
-        if ($(window).scrollTop() > 10) {
+        if ($(window).scrollTop() > 80) {
             $('#header').addClass('header-fixed');
         } else {
             $('#header').removeClass('header-fixed');
         }
+    });
+
+
+    /* Tabs */
+
+    $('.person').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('.person').removeClass('.current');
+        $('.person-about').removeClass('.current');
+
+        $(this).addClass('.current');
+        $('#'+tab_id).addClass('.current');
     });
 
 
